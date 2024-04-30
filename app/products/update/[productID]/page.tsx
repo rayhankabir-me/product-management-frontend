30b9c9a37070f6ea5934c0ae6f17beda9f11f860
rayhankabir-me <devrayhankabir@gmail.com>
2024-04-30 19:05:49 +0600

    fetching data using useEffect in update page

--- app/products/update/[productID]/page.tsx
+++ app/products/update/[productID]/page.tsx
@@ -1,7 +1,151 @@
-export default async function Update({
-  params,
-}: {
-  params: { productID: string };
-}) {
-  return <h1>updating product - {params.productID}</h1>;
+"use client";
+
+import axios from "axios";
+import { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
+
+export default function Update({ params }: { params: { productID: string } }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Run once on component mount
+
+  async function fetchData() {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/products/${params.productID}`
+      );
+      const data = response.data;
+      setTitle(data.title);
+      setDescription(data.description);
+      setImage(data.image);
+      setPrice(data.price);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("Error fetching data");
+    }
+  }
+
+  const handleTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDescription = (e: ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
+  };
+
+  const handleImage = (e: ChangeEvent<HTMLInputElement>) => {
+    setImage(e.target.value);
+  };
+
+  const handlePrice = (e: ChangeEvent<HTMLInputElement>) => {
+    setPrice(e.target.value);
+  };
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    if (!title || !description || !image || !price) {
+      setError("All fields are required");
+    } else {
+      try {
+        await postData();
+        setError("Product created successfully");
+        setTitle("");
+        setDescription("");
+        setImage("");
+        setPrice("");
+      } catch (error) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  async function postData() {
+    try {
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("description", description);
+      formData.append("image", image);
+      formData.append("price", price);
+
+      const response = await axios.post(
+        "http://localhost:3000/products/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="py-40 px-40">
+      <h3 className="mb-10">Create Product</h3>
+      <form onSubmit={handleSubmit}>
+        <label className="block mb-3" htmlFor="title">
+          Product Title
+        </label>
+        <input
+          className="py-2 px-3 mb-5 w-1/2 text-black"
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          onChange={handleTitle}
+        />
+        <label className="block mb-3" htmlFor="description">
+          Product Description
+        </label>
+        <textarea
+          className="py-2 px-3 mb-5 w-1/2 text-black"
+          type="text"
+          name="description"
+          id="description"
+          value={description}
+          onChange={handleDescription}
+        />
+        <label className="block mb-3" htmlFor="price">
+          Product Price
+        </label>
+        <input
+          className="py-2 px-3 mb-5 w-1/2 text-black"
+          type="text"
+          name="price"
+          id="price"
+          value={price}
+          onChange={handlePrice}
+        />
+        <label className="block mb-3" htmlFor="image">
+          Image Url
+        </label>
+        <input
+          className="py-2 px-3 mb-5 w-1/2 text-black"
+          type="text"
+          name="image"
+          id="image"
+          value={image}
+          onChange={handleImage}
+        />
+
+        {error && <p>{error}</p>}
+        <button
+          className="block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+          type="submit"
+        >
+          Create
+        </button>
+      </form>
+    </div>
+  );
 }
